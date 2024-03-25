ce33215d24dc64da5017adf09b22d25948f00e15
Bharat8087 <157899792+Bharat8087@users.noreply.github.com>
2024-03-25 21:24:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ function isSameType(value1, value2) {
     if (isNaN(value1) && isNaN(value2)) {
         return true;
     }
+	    if (isNaN(value1) || isNaN(value2)) {
+        return false;
+    }
+
 
     return typeof value1 === typeof value2;
 }
